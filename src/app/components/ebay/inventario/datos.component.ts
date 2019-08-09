063075170deb051922819f7694d725a735b8d36c
gavialus <gavialus@gmail.com>
2019-08-09 14:00:19 +0200

    separacion de la apirest

--- src/app/components/ebay/inventario/datos.component.ts
+++ src/app/components/ebay/inventario/datos.component.ts
@@ -5,9 +5,10 @@ import {
   FormControl,
   Validators,
   FormArray,
+  FormBuilder,
   NgForm
 } from "@angular/forms";
-import { interproductos } from "src/app/models/termino";
+import { interproductos,tallas } from "src/app/models/termino";
 import { InventarioService } from "src/app/service/inventario.service";
 import Swal from "sweetalert2";
 import { Observable } from "rxjs";
@@ -20,10 +21,13 @@ import { ActivatedRoute } from "@angular/router";
 export class DatosComponent implements OnInit {
   forma: FormGroup;
   producto = new interproductos();
+  productos = [];
   imagenes = [];
- 
+  tallas:FormArray
 
-  constructor(private inv: InventarioService, private route: ActivatedRoute) {
+  constructor(private inv: InventarioService, private route: ActivatedRoute, private fb:FormBuilder) {
+  }
+  ngOnInit() {
     this.forma = new FormGroup({
       id: new FormControl(this.producto.id),
       //'accion': new FormControl('Add'),
@@ -32,46 +36,53 @@ export class DatosComponent implements OnInit {
       precio: new FormControl(""),
       categoria: new FormControl(""),
       caracteristicas: new FormControl(""),
-      imagenes: new FormArray([new FormControl("")])
+      fecha: new FormControl(""),
+      imagenes: new FormArray([new FormControl("")]),
+      preCompra: new FormControl(""),
+      comiPay: new FormControl("0"),
+      comiEbay: new FormControl("0"),
+      portes: new FormControl(),
+      margen: new FormControl(this.producto.margen),
+      tallas: this.fb.array([
+        this.crearTalla()
+      ])
     });
-  }
-  ngOnInit() {
+
+
     const id = this.route.snapshot.paramMap.get("id");
     
     if (id !== "nuevo") {
       this.inv.getProducto(id).subscribe((resp: interproductos) => {
         this.producto = resp;
-        console.log(this.producto);
-        this.imagenes= resp.imagenes
-        console.log(this.imagenes);
+        this.productos.push(resp);
+       // this.tallas=resp.tallas
         
+        console.log(this.producto);
+        this.imagenes = resp.imagenes;
+        console.log(this.imagenes);
+
         this.producto.id = id;
         this.forma.patchValue(this.producto);
 
-            const pictures = this.forma.get('imagenes') as FormArray;
-            console.log(pictures.value);
-            while (pictures.length) {
-              pictures.removeAt(0);
-            }
-            console.log(pictures.value);
-
-            resp.imagenes.forEach(picture=>pictures.push(new FormControl(picture)))
-
-            console.log(pictures.value);
+        const pictures = this.forma.get("imagenes") as FormArray;
+        console.log(pictures.value);
+        while (pictures.length) {
+          pictures.removeAt(0);
+        }
+        console.log(pictures.value);
+        resp.imagenes.forEach(picture =>
+          pictures.push(new FormControl(picture))
+        );
+        console.log(pictures.value);
       });
-      
-      
-      
-
-
     }
   }
 
   enviar(forma: NgForm) {
     if (this.forma.invalid) {
-      console.log("formulario no valido");
       return;
     }
+    console.log("formulario no valido", this.forma);
 
     Swal.fire({
       title: "Espere por favor",
@@ -100,4 +111,33 @@ export class DatosComponent implements OnInit {
   agregarImagen() {
     (<FormArray>this.forma.controls["imagenes"]).push(new FormControl(""));
   }
+  borrarImagen(i: number) {
+    (<FormArray>this.forma.controls["imagenes"]).removeAt(i);
+  }
+
+ crearTalla():FormGroup{
+   return this.fb.group({
+    talla:[""],
+    cantidad:[""]
+
+   })
+
+ }
+
+  agregartalla():void {
+
+    this.tallas=this.forma.get('tallas') as FormArray
+    this.tallas.push(this.crearTalla())
+  
+      
+    
+
+    
+    
+   // (<FormArray>this.forma.controls["tallas"]).push(new FormControl(""))
+
+  }
+
+    //this.forma.controls["precio"].value(new FormControl); 
+  
 }
--- src/app/models/termino.ts
+++ src/app/models/termino.ts
@@ -3,13 +3,30 @@ export class interproductos {
     'accion': string;
     'titulo': string;
     'marca': string;
-    'precio': string;
+    'precio': number;
+    'preciocom':number;
     'categoria': string;
     'caracteristicas': string;
     'imagenes': [];
-    'Imagenes': any
+    'Imagenes': any;
+    'fecha':Date;
+    'comiEbay': number;
+    'comiPay': number ;
+    'preCompra':number;
+    'margen':number;
+    'portes': number;
+    'tallas':[]
 }
 
-export class picture{
-    'pruebaimg':string
+export class tallas{
+    '34':number;
+    '35':number;
+    '36':number;
+    '37':number;
+    '38':number;
+    '39':number;
+    '40':number;
+    '41':number;
+    '42':number;
+    
 }
